dc01b719bb5ca7a7ade8623569d85752f2b291f9
Dave Lu <daveylupie@gmail.com>
2022-11-28 21:05:47 -0500

    Creation of better boundary conditions
    Better conditions for when buckets hit zero. Still not perfect, as there are still people being created out of nowhere.

--- project.py
+++ project.py
@@ -8,32 +8,53 @@ def dS(b1, b2, b3, i1, i2, i3, S, k, dt, dw1, dw2, dw3):
     part2 = -k*(dw1*math.sqrt(b1*i1*S) + dw2*math.sqrt(b2*i2*S) + dw3*math.sqrt(b3*i3*S))
     return part1 + part2
 
-def dE(b1, b2, b3, i1, i2, i3, a, E, S, k, dt, dw1, dw2, dw3):
-    part1 = (b1*i1 + b2*i2 + b3*i3)*S*dt -a*E*dt
-    part2 = k*(dw1*math.sqrt(b1*i1*S) + dw2*math.sqrt(b2*i2*S) + dw3*math.sqrt(b3*i3*S))
-    return part1 + part2
+def dE(b1, b2, b3, i1, i2, i3, a, E, S, k, dt, dw1, dw2, dw3, dw4, S_floored):
+    if(S_floored == True): S_part = S
+    else:
+        S_part = (b1*i1 + b2*i2 + b3*i3)*S*dt + k*(dw1*math.sqrt(b1*i1*S) + dw2*math.sqrt(b2*i2*S) + dw3*math.sqrt(b3*i3*S))
+    E_part = -a*E*dt - (dw4*math.sqrt(a*E))
+    return S_part + E_part
+
+def dI1(a, E, y1, p1, dt, i1, k, dw4, dw5, dw6, E_floored):
+    if(E_floored == True):
+        E_part = E
+    else:
+        E_part = a*E*dt + k*dw4*math.sqrt(a*E)
+    i1_part = -(y1+p1)*i1*dt - k*(dw5*math.sqrt(y1*i1) + dw6*math.sqrt(p1*i1))
+    return E_part + i1_part
+
+def dI2(p1, y1, y2, p2, dt, i1, i2, k, dw6, dw7, dw8, i1_floored):
+    if(i1_floored == True):
+        i1_part = i1 * (p1 / (y1 + p1))
+    else:
+        i1_part = p1*i1 + k*dw6*math.sqrt(p1*i1)
+    i2_part = -(y2+p2)*i2*dt - k*(-dw7*math.sqrt(y2*i2) - dw8*math.sqrt(p2*i2))
+    return i1_part + i2_part
+
+def dI3(y2, p2, i2, y3, u, dt, i3, k, dw8, dw9, dw10, i2_floored):
+    if(i2_floored == True):
+        i2_part = i2 * (p2 / (y2 + p2))
+    else:
+        i2_part = p2*i2*dt + k*dw8*math.sqrt(p2*i2)
+    i3_part = -(y3+u)*i3*dt + k*(-dw9*math.sqrt(y3*i3) - dw10*math.sqrt(u*i3))
+    return i2_part + i3_part
+
+def dR(y1, y2, y3, p1, p2, u, i1, i2, i3, k, dt, dw5, dw7, dw9, i1_floored, i2_floored, i3_floored):
+    if(i1_floored == True): i1_part = i1 * (y1 / (y1 + p1))
+    else: i1_part = y1*i1*dt + k*dw5*math.sqrt(y1*i1)
+
+    if(i2_floored == True): i2_part = i2 * (y2 / (y2 + p2))
+    else: i2_part = y2*i2*dt + k*dw7*math.sqrt(y2*i2)
+
+    if(i3_floored == True): i3_part = i3 * (y3 / (y3 + u))
+    else: i3_part = y3*i3*dt + k*dw9*math.sqrt(y3*i3)
+    # part1 = (y1*i1 + y2*i2 + y3*i3)*dt
+    # part2 = k*(dw5*math.sqrt(y1*i1) + dw7*math.sqrt(y2*i2) + dw9*math.sqrt(y3*i3))
+    return i1_part + i2_part + i3_part
     
-def dI1(a, E, y1, p1, dt, i1, k, dw4, dw5, dw6):
-    part1 = a*E*dt - (y1+p1)*i1*dt
-    part2 = k*(dw4*math.sqrt(a*E) - dw5*math.sqrt(y1*i1) - dw6*math.sqrt(p1*i1))
-    return part1 + part2
-
-def dI2(p1, y2, p2, dt, i1, i2, k, dw6, dw7, dw8):
-    part1 = p1*i1 - (y2+p2)*i2*dt
-    part2 = k*(dw6*math.sqrt(p1*i1) - dw7*math.sqrt(y2*i2) - dw8*math.sqrt(p2*i2))
-    return part1 + part2
-
-def dI3(y2, p2, i2, y3, u, dt, i3, k, dw8, dw9, dw10):
-    part1 = p2*i2*dt - (y3+u)*i3*dt
-    part2 = k*(dw8*math.sqrt(p2*i2) - dw9*math.sqrt(y3*i3) - dw10*math.sqrt(u*i3))
-    return part1 + part2
-
-def dR(y1, y2, y3, i1, i2, i3, k, dt, dw5, dw7, dw9):
-    part1 = (y1*i1 + y2*i2 + y3*i3)*dt
-    part2 = k*(dw5*math.sqrt(y1*i1) + dw7*math.sqrt(y2*i2) + dw9*math.sqrt(y3*i3))
-    return part1 + part2
-    
-def dD(i3, u, k, dt, dw10):
+def dD(i3, y3, u, k, dt, dw10, i3_floored):
+    if(i3_floored == True):
+        return i3 * (u / (y3 + u))
     return (u*i3*dt) + k*dw10*math.sqrt(u*i3)
     
 def main(days):
@@ -51,13 +72,13 @@ def main(days):
     b1 = 0.5
     b2 = 0.1
     b3 = 0.1
-    y1 = 0.133  
     p1 = 0.033
     p2 = 0.042
+    y1 = 0.133  
     y2 = 0.125
-    u = 0.050
     y3 = 0.075
-    dt = 0.5
+    u = 0.050
+    dt = 1
     k = 0
 
     # The parametres can be changed based upon the severity of PHSM (Public Health and Safety Measures)
@@ -73,6 +94,7 @@ def main(days):
     runs = int(days / dt)
     for i in range(1, runs + 1):
         
+        #used to create noise/stochasticity
         dw1 = random.normal(0, 1)
         dw2 = random.normal(0, 1)
         dw3 = random.normal(0, 1)
@@ -84,51 +106,69 @@ def main(days):
         dw9 = random.normal(0, 1)
         dw10 = random.normal(0, 1)
 
-        change_S= dS(b1, b2, b3, i1, i2, i3, S, k, dt, dw1, dw2, dw3)
-        change_E = dE(b1, b2, b3, i1, i2, i3, a, E, S, k, dt, dw1, dw2, dw3)
-        change_R = dR(y1, y2, y3, i1, i2, i3, k, dt, dw5, dw7, dw9)
-        change_i1 = dI1(a, E, y1, p1, dt, i1, k, dw4, dw5, dw6)
-        change_i2 = dI2(p1, y2, p2, dt, i1, i2, k, dw6, dw7, dw8)
-        change_i3 = dI3(y2, p2, i2, y3, u, dt, i3, k, dw8, dw9, dw10)
-        change_D = dD(i3, u, k, dt, dw10)
+        #needed to keep track of whether that bucket drops to zero in a step
+        S_floored = False
+        E_floored = False
+        i1_floored = False
+        i2_floored = False
+        i3_floored = False
 
+        #holds copies of initial/original values for calculations
+        #all calculations should be done with these vars so everything occurs "at once"
+        init_S = S
+        init_E = E
+        init_i1 = i1
+        init_i2 = i2
+        init_i3 = i3
+        
+        change_S= dS(b1, b2, b3, init_i1, init_i2, init_i3, init_S, k, dt, dw1, dw2, dw3)
         if(S + change_S < 0):
             E += S
             S = 0
+            S_floored = True
         else:
             S += change_S
 
-        if(E + change_E < 0):
-            i1 += E
-            E = 0
+        change_E = dE(b1, b2, b3, init_i1, init_i2, init_i3, a, init_E, init_S, k, dt, dw1, dw2, dw3, dw4, S_floored)
+        if(init_E + change_E < 0): #amount of E at start of step drops to zero
+            i1 += init_E #move it all to i1 since that's how the model works
+            E -= init_E #remove initial amount from current E.
+            E_floored = True
         else:
             E += change_E
 
-        if(i1 + change_i1 < 0):
-            i2 += i1 * (p1 / (y1 + p1))
-            R += i1 * (y1 / (y1 + p1))
-            i1 = 0
+        change_i1 = dI1(a, init_E, y1, p1, dt, init_i1, k, dw4, dw5, dw6, E_floored)
+        if(init_i1 + change_i1 < 0):
+            i2 += init_i1 * (p1 / (y1 + p1))
+            R += init_i1 * (y1 / (y1 + p1))
+            i1 -= init_i1
+            i1_floored = True
         else:
             i1 += change_i1
 
-        if(i2 + change_i2 < 0):
-            i3 += i2 * (p2 / (y2 + p2))
-            R += i2 * (y2 / (y2 + p2))
-            i2 = 0
+        change_i2 = dI2(p1, y1, y2, p2, dt, init_i1, init_i2, k, dw6, dw7, dw8, i1_floored)
+        if(init_i2 + change_i2 < 0):
+            i3 += init_i2 * (p2 / (y2 + p2))
+            R += init_i2 * (y2 / (y2 + p2))
+            i2 -= init_i2
+            i2_floored = True
         else:
             i2 += change_i2
 
-        if(i3 + change_i3 < 0):
-            R += i3 * (y3 / (y3 + u))
-            D += i3 * (u / (y3 + u))
-            change_D = 0
-            i3 = 0
+        change_i3 = dI3(y2, p2, init_i2, y3, u, dt, init_i3, k, dw8, dw9, dw10, i2_floored)
+        if(init_i3 + change_i3 < 0):
+            R += init_i3 * (y3 / (y3 + u))
+            D += init_i3 * (u / (y3 + u))
+            i3 -= init_i3
+            i3_floored = True
         else:
             i3 += change_i3
 
-        #TODO: create better way to deal with change_R when i1, i2, i3 end up at 0
+        #TODO: create better way to deal with change_R when i1, i2, or i3 end up at 0
+        change_R = dR(y1, y2, y3, p1, p2, u, i1, i2, i3, k, dt, dw5, dw7, dw9, i1_floored, i2_floored, i3_floored)
         R += change_R
 
+        change_D = dD(i3, y3, u, k, dt, dw10, i3_floored)
         D += change_D
 
         vals_S.append(S)
